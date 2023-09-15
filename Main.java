bd9eb4985b1de5e4288373dd6a8504b4184b096e
Maxim <73311330+hovercod@users.noreply.github.com>
2023-09-14 18:32:07 -0700

    JAVA Args Example

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args)
+
+    {
+
+
+        if (args [0].equals ("-h"))
+        System.out.println("Hello");
+        else if (args [0].equals ("-g"))
+        System.out.println("Goodbye, ");
+     
+
+        for (int i = 1; i < args.length ; i++) {
+            System.out.println("" + args[i]);
+            System.out.print("!");
+        }
+
+
+
+
+    }
+}
