534dea0c0541f921f2ed7c755323991e200b148e
Maxim <73311330+hovercod@users.noreply.github.com>
2023-09-20 00:16:11 -0700

    whiledowhile

--- Main.java
+++ Main.java
@@ -1,21 +1,42 @@
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
+// safe money //
+
+import java.util.Objects;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+
+        int balance = 0;
+        int payment = 6000;
+        int goal = 1000000;
+        int interesRate = 5;
+        int years = 42;
 
 
-        Scanner in = new Scanner(Path.of("C:\\Example\\pokemon.txt"), StandardCharsets.UTF_8);
+        while (years < 59) {
+            balance += payment;
+            double interest = balance * interesRate / 100;
+            balance += interest;
+            years++;
+        }
 
-        PrintWriter out = new PrintWriter("C:\\Example\\newpokemon.txt", StandardCharsets.UTF_8);
 
-        String dir = System.getProperty("User111.dir");
+        String net = "n";
+        String answer;
+
+        do {
+            balance += payment;
+            double interest = balance * interesRate / 100;
+            balance += interest;
+            years++;
+            System.out.println(years + "  you are want to go to rest? Your balans " + balance);
+            Scanner inp = new Scanner(System.in);
+            answer = inp.next();
+        }
+
+        while (Objects.equals(answer, net));
+        System.out.println("Your old " + years + " and your balance " + balance);
 
     }
+
 }
