5cec4f6002bc8ce233e667c668aea445a723f6d2
Maxim <73311330+hovercod@users.noreply.github.com>
2023-09-20 00:13:02 -0700

    txtfilereader

--- Main.java
+++ Main.java
@@ -1,23 +1,21 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-
-        final int NMAX = 10;
-
-        int[][] odds = new int[NMAX + 1][];
-
-        for (int n = 0; n <= NMAX; n++)
-            odds[n] = new int[n + 1];
-
-     /*   for (int n = 0; n < odds.length ; n++)
-            for (int k = 0; k < odds[n].length ; k++)*/
+    public static void main(String[] args) throws IOException {
 
 
+        Scanner in = new Scanner(Path.of("C:\\Example\\pokemon.txt"), StandardCharsets.UTF_8);
 
+        PrintWriter out = new PrintWriter("C:\\Example\\newpokemon.txt", StandardCharsets.UTF_8);
 
-        for (int [] row : odds) {
-            for (int odd : row)
-                System.out.printf("%4d", odd);
-            System.out.println();
-        }
+        String dir = System.getProperty("User111.dir");
+
     }
 }
