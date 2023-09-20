27ccad020375697b9093bf37aa37e384cd044cd7
Maxim <73311330+hovercod@users.noreply.github.com>
2023-09-20 00:09:07 -0700

    CompoundInterest

--- Main.java
+++ Main.java
@@ -1,95 +1,47 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
 
-        // example While
+        System.out.println("Hello world!");
 
-        double balance = 2;
-        double goal = 10;
-        double payment = 1;
-        double interestRate = 5;
-        int years = 20;
+        final double STARRATE = 10;
+        final int NRATES = 6;
+        final int NYEARS = 10;
 
-        while (balance < goal)
-        {
-            balance += payment;
-            double interest = balance * interestRate / 100;
-            balance += interest;
-            years++;
-        }
-        System.out.println("old = " + years + ", balance = " + balance);
-
-        //  example Do While
-
-        double balanceDo = 2;
-        double goalDo = 10;
-        double paymentDo = 1;
-        double interestRateDo = 5;
-        int yearsDo = 20;
-
-        do
-        {
-            balanceDo += paymentDo;
-            double interestDo = balanceDo * interestRateDo / 100;
-            balanceDo += interestDo;
-            yearsDo++;
-        }
-        while (balanceDo < goalDo);
-        System.out.println("old = " + yearsDo + ", balance = " + balanceDo);
-
-        // example While
-
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("How much money do you " + "need to retire?");
-        double goalExample = in.nextDouble();
-
-        System.out.println("How much money will you " + "contribute every year?");
-        double paymentExample = in.nextDouble();
-
-        System.out.println("Interest rate in %: ");
-        double interestRateExample = in.nextDouble();
-
-        double balanceExample = 0;
-        int yearsExample = 0;
-
-        while (balanceExample < goalExample)
-        {
-            balanceExample += paymentExample;
-            double interestExample = balance * interestRate / 100;
-            balanceExample += interestExample;
-            yearsExample ++;
+        double[] interestRate = new double[NRATES];
+        for (int j = 0; j < interestRate.length; j++) {
+            interestRate[j] = (STARRATE + j) / 100.0;
         }
 
-        System.out.println("You can retire in " + yearsExample + " years");
+        double[][] balance = new double[NYEARS][NRATES];
 
-        Scanner inExam = new Scanner(System.in);
-        System.out.println("How much money will you contribute every year?");
-        double paymentExam = inExam.nextDouble();
-
-        System.out.println("Interest rate in%:");
-        double interestRateExam = inExam.nextDouble();
-
-        double balanceExam = 0;
-        int yearExam = 0;
-        int input;
-
-        do
-        {
-            balanceExam += paymentExam;
-            double interestExam = balanceExam * interestRateExam / 100;
-            balanceExam += interestExam;
-
-            yearExam ++;
-
-            System.out.printf("After year %d, your balance is %,.2f%n", yearExam, balance);
-            System.out.println("Ready to retire? (Y=1/N=2)");
-            input = inExam.nextInt();
+        for (int j = 0; j < balance[0].length; j++) {
+            balance[0][j] = 10000;
         }
-        while (input == 2);
+
+        for (int i = 1; i < balance.length; i++) {
+            for (int j = 0; j < balance[i].length; j++) {
+
+                double oldBalance = balance[i - 1][j];
+
+                double interest = oldBalance * interestRate[j];
+
+                balance[i][j] = oldBalance + interest;
+            }
+        }
+
+        for (int j = 0; j < interestRate.length; j++) {
+            System.out.printf("%9.0f%%", 100 * interestRate[j]);
+            System.out.print("");
+        }
+
+        System.out.println();
+
+        for (double[] a : balance) {
+            for (double b : a) {
+                System.out.printf("%10.2f", b);
+            }
+            System.out.println();
+        }
 
     }
 
