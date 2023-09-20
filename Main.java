4728e77ac24ecb2b6a8e85c963e89ef4e0a1cf25
Maxim <73311330+hovercod@users.noreply.github.com>
2023-09-20 00:12:28 -0700

    trianglearray

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        final int NMAX = 10;
+
+        int[][] odds = new int[NMAX + 1][];
+
+        for (int n = 0; n <= NMAX; n++)
+            odds[n] = new int[n + 1];
+
+     /*   for (int n = 0; n < odds.length ; n++)
+            for (int k = 0; k < odds[n].length ; k++)*/
+
+
+
+
+        for (int [] row : odds) {
+            for (int odd : row)
+                System.out.printf("%4d", odd);
+            System.out.println();
+        }
     }
 }
