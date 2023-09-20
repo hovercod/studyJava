e66c9914fb5babda0ab0efbad0493b6ea5c50c90
Maxim <73311330+hovercod@users.noreply.github.com>
2023-09-20 00:10:48 -0700

    switchfori

--- Main.java
+++ Main.java
@@ -1,48 +1,49 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println(" Select 1,2,3,4");
+        int choise = in.nextInt();
 
-        final double STARRATE = 10;
-        final int NRATES = 6;
-        final int NYEARS = 10;
-
-        double[] interestRate = new double[NRATES];
-        for (int j = 0; j < interestRate.length; j++) {
-            interestRate[j] = (STARRATE + j) / 100.0;
+        switch (choise)
+        {
+            case 1:
+                System.out.println("111");
+                break;
+            case 2:
+                System.out.println("222");
+                break;
+            case 3:
+                System.out.println("333");
+                break;
+            case 4:
+                System.out.println("444");
+                break;
+            default:
+                System.out.println("error!!!");
+                break;
         }
 
-        double[][] balance = new double[NYEARS][NRATES];
+        Scanner world = new Scanner(System.in);
+        System.out.println("write yes or no");
+        String input = world.next();
 
-        for (int j = 0; j < balance[0].length; j++) {
-            balance[0][j] = 10000;
-        }
-
-        for (int i = 1; i < balance.length; i++) {
-            for (int j = 0; j < balance[i].length; j++) {
-
-                double oldBalance = balance[i - 1][j];
-
-                double interest = oldBalance * interestRate[j];
-
-                balance[i][j] = oldBalance + interest;
-            }
-        }
-
-        for (int j = 0; j < interestRate.length; j++) {
-            System.out.printf("%9.0f%%", 100 * interestRate[j]);
-            System.out.print("");
-        }
-
-        System.out.println();
-
-        for (double[] a : balance) {
-            for (double b : a) {
-                System.out.printf("%10.2f", b);
-            }
-            System.out.println();
+        switch (input.toLowerCase())
+        {
+            case "yes":
+                System.out.println("Yes you win");
+                break;
+            case "no":
+                System.out.println("No try again");
+                break;
+            default:
+                System.out.println("error");
+                break;
         }
 
     }
-
 }
